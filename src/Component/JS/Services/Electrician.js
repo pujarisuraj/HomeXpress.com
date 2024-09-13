79204cea59d859ca1a996af87f6108d4469a2400
SURAJ PUAJRI <surajpujari8383@gmail.com>
2024-09-13 15:15:22 +0530

    Img Added

--- src/Component/JS/Services/Electrician.js
+++ src/Component/JS/Services/Electrician.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import '../../CSS/Services/Ac.css';
-import AcAdImag1 from '../../Assets/Img/AcAdImg/1.png'
-import AcAdImag2 from '../../Assets/Img/AcAdImg/2.png'
-import AcAdImag3 from '../../Assets/Img/AcAdImg/3.png'
-import AcAdImag4 from '../../Assets/Img/AcAdImg/4.png'
+import AcAdImag1 from '../../Assets/Img/Electricain/1.png'
+import AcAdImag2 from '../../Assets/Img/Electricain/2.png'
+import AcAdImag3 from '../../Assets/Img/Electricain/3.png'
+import AcAdImag4 from '../../Assets/Img/Electricain/4.png'
+import AcAdImag5 from '../../Assets/Img/Electricain/5.png'
+import AcAdImag6 from '../../Assets/Img/Electricain/6.png'
 
 function Electrician() {
     return (
@@ -29,10 +31,10 @@ function Electrician() {
 
                                 <div className='price'>
                                     <span class="material-symbols-outlined main-price">currency_rupee</span>
-                                    <span className='main-price'>999</span>
+                                    <span className='main-price'>1199</span>
                                     <span class="material-symbols-outlined span3 ">currency_rupee </span>
-                                    <span className='span4'><strike>2,000</strike></span>
-                                    <span className='span5'>-20% off</span>
+                                    <span className='span4'><strike>2,999</strike></span>
+                                    <span className='span5'>-60% off</span>
                                 </div>
                             </div>
                         </div>
@@ -47,10 +49,10 @@ function Electrician() {
 
                                 <div className='price'>
                                     <span class="material-symbols-outlined main-price">currency_rupee</span>
-                                    <span className='main-price'>999</span>
+                                    <span className='main-price'>200</span>
                                     <span class="material-symbols-outlined span3 ">currency_rupee</span>
-                                    <span className='span4'><strike>2,000</strike></span>
-                                    <span className='span5'>-20% off</span>
+                                    <span className='span4'><strike>299</strike></span>
+                                    <span className='span5'>-33% off</span>
                                 </div>
                             </div>
 
@@ -65,10 +67,10 @@ function Electrician() {
                                 <p className='service-name'>Computer & Television </p>
                                 <div className='price'>
                                     <span class="material-symbols-outlined main-price">currency_rupee</span>
-                                    <span className='main-price'>999</span>
+                                    <span className='main-price'>599</span>
                                     <span class="material-symbols-outlined span3 ">currency_rupee</span>
-                                    <span className='span4'><strike>2,000</strike></span>
-                                    <span className='span5'>-20% off</span>
+                                    <span className='span4'><strike>999</strike></span>
+                                    <span className='span5'>-40% off</span>
                                 </div>
                             </div>
 
@@ -83,10 +85,10 @@ function Electrician() {
                                 <p className='service-name'>MCB & Fuse</p>
                                 <div className='price'>
                                     <span class="material-symbols-outlined main-price">currency_rupee</span>
-                                    <span className='main-price'>999</span>
+                                    <span className='main-price'>249</span>
                                     <span class="material-symbols-outlined span3 ">currency_rupee</span>
-                                    <span className='span4'><strike>2,000</strike></span>
-                                    <span className='span5'>-20% off</span>
+                                    <span className='span4'><strike>499</strike></span>
+                                    <span className='span5'>-50% off</span>
                                 </div>
                             </div>
 
@@ -94,17 +96,17 @@ function Electrician() {
 
                         <div className="service-box">
                             <div className="service-img">
-                                <img src={AcAdImag4} alt="" />
+                                <img src={AcAdImag5} alt="" />
                             </div>
                             <div className='serv-info'>
                                 <p className='rating'><i className="ri-star-fill rating-star"> 4.2</i></p>
-                                <p className='service-name'>Fan </p>
+                                <p className='service-name'>Fan Service</p>
                                 <div className='price'>
                                     <span class="material-symbols-outlined main-price">currency_rupee</span>
-                                    <span className='main-price'>999</span>
+                                    <span className='main-price'>989</span>
                                     <span class="material-symbols-outlined span3 ">currency_rupee</span>
-                                    <span className='span4'><strike>2,000</strike></span>
-                                    <span className='span5'>-20% off</span>
+                                    <span className='span4'><strike>1,499</strike></span>
+                                    <span className='span5'>-34% off</span>
                                 </div>
                             </div>
 
@@ -112,18 +114,18 @@ function Electrician() {
 
                         <div className="service-box">
                             <div className="service-img">
-                                <img src={AcAdImag4} alt="" />
+                                <img src={AcAdImag6} alt="" />
                             </div>
                             <div className='serv-info'>
                                 <p className='rating'><i className="ri-star-fill rating-star"> 4.2</i></p>
-                                <p className='service-name'>Looking for something else?</p>
+                                <p className='service-name'>Looking for Something else?</p>
 
                                 <div className='price'>
                                     <span class="material-symbols-outlined main-price">currency_rupee</span>
-                                    <span className='main-price'>999</span>
+                                    <span className='main-price'>91</span>
                                     <span class="material-symbols-outlined span3 ">currency_rupee</span>
-                                    <span className='span4'><strike>2,000</strike></span>
-                                    <span className='span5'>-20% off</span>
+                                    <span className='span4'><strike>457</strike></span>
+                                    <span className='span5'>-80% off</span>
                                 </div>
                             </div>
 
